812f997abe46d05510aa963b2b1c6539a13b9e5c
GlanvilleG <gerardglanville@icloud.com>
2026-07-06 15:10:57 +1200

    Initial Commit

---
+++ GolfShopPro Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GolfShopPro Watch App
+//
+//  Created by Dragon Development on 06/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
