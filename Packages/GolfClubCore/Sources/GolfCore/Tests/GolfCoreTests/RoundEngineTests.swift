a3a1c384275004f91717f877955af078768d1380
GlanvilleG <gerardglanville@icloud.com>
2026-07-10 08:37:19 +1200

    test: add round engine workflow tests

---
+++ Packages/GolfClubCore/Sources/GolfCore/Tests/GolfCoreTests/RoundEngineTests.swift
@@ -0,0 +1,17 @@
+//
+//  RoundEngineTests.swift
+//  GolfCore
+//
+//  Created by Dragon Development on 10/07/2026.
+//
+
+startRound
+confirmTeeSet
+confirmHole
+announceClub
+changeClub
+recordShotFeedback
+announce next club completes previous shot
+recordPutts
+leaveHolePending
+finishRound
